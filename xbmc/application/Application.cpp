6a2cc08662381b31b5e692299a0cc1d9ddcefa45
axbmcuser <mnhuetbu2348234sjjmql3734@sharklasers.com>
2026-05-15 22:28:47 +0200

    backport upnp fixes #27521

--- xbmc/application/Application.cpp
+++ xbmc/application/Application.cpp
@@ -1553,6 +1553,7 @@ void CApplication::OnApplicationMessage(ThreadMessage* pMsg)
 
   case TMSG_RESUMEAPP:
   {
+    m_ServiceManager->GetNetwork().NetworkMessage(CNetworkBase::SERVICES_UP, 0);
     CGUIComponent* gui = CServiceBroker::GetGUI();
     if (gui)
       gui->GetWindowManager().MarkDirty();
